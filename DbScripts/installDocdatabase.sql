d06f9758bb1591fe42a8d38bf1b0a8fb40c0cb70
wkohusjr <wkohusjr@moeller.org>
2014-04-04 01:00:17 -0400

    docDatabaseSQLNEW
    Full script of Bill's database.  1:00 AM Thursday.

--- DbScripts/installDocdatabase.sql
+++ DbScripts/installDocdatabase.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.9
+-- version 3.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 04, 2014 at 02:57 AM
--- Server version: 5.5.34
--- PHP Version: 5.4.22
+-- Host: localhost
+-- Generation Time: Apr 04, 2014 at 04:58 AM
+-- Server version: 5.5.24-log
+-- PHP Version: 5.4.3
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -19,33 +19,45 @@ SET time_zone = "+00:00";
 --
 -- Database: `docdatabase`
 --
-CREATE DATABASE IF NOT EXISTS `docdatabase` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `docdatabase`;
 
 DELIMITER $$
 --
 -- Functions
 --
 DROP FUNCTION IF EXISTS `GetCourseName`$$
-CREATE DEFINER=`root`@`localhost` FUNCTION `GetCourseName`(cID INT) RETURNS varchar(100) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER
+CREATE DEFINER=`root`@`localhost` FUNCTION `GetCourseName`(`cID` INT) RETURNS varchar(100) CHARSET latin1
+    DETERMINISTIC
 BEGIN
-  DECLARE cName VARCHAR(100);
-  SELECT courseName 
-  INTO cName
-  FROM courses 
-  WHERE courseID = cID;
-  RETURN cName;
+DECLARE cName VARCHAR(100);
+SELECT courseName
+INTO cName
+FROM courses
+WHERE courseID = cID;
+RETURN cName;
 END$$
 
 DROP FUNCTION IF EXISTS `GetDeptName`$$
-CREATE DEFINER=`root`@`localhost` FUNCTION `GetDeptName`(deptId INT) RETURNS varchar(100) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER
+CREATE DEFINER=`root`@`localhost` FUNCTION `GetDeptName`(`dID` INT) RETURNS varchar(100) CHARSET latin1
+    DETERMINISTIC
 BEGIN
-  DECLARE dName VARCHAR(100);
-  SELECT deptName 
-  INTO dName
-  FROM departments 
-  WHERE deptID = deptId;
-  RETURN dName;
+DECLARE dName VARCHAR(100);
+SELECT deptName
+INTO dName
+FROM departments
+WHERE deptID = dID;
+RETURN dName;
+END$$
+
+DROP FUNCTION IF EXISTS `GetUserName`$$
+CREATE DEFINER=`root`@`localhost` FUNCTION `GetUserName`(`uID` INT) RETURNS text CHARSET latin1
+    DETERMINISTIC
+BEGIN
+DECLARE uName TEXT;
+SELECT fName
+INTO uName
+FROM users
+WHERE userID = uID;
+RETURN uName;
 END$$
 
 DELIMITER ;
@@ -63,7 +75,24 @@ CREATE TABLE IF NOT EXISTS `courses` (
   `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updateDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`courseID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`courseID`, `courseName`, `createDate`, `updateDate`) VALUES
+(1, 'Biology 101', '2014-04-04 01:50:16', '0000-00-00 00:00:00');
+
+--
+-- Triggers `courses`
+--
+DROP TRIGGER IF EXISTS `courses_creation_timestamp`;
+DELIMITER //
+CREATE TRIGGER `courses_creation_timestamp` BEFORE INSERT ON `courses`
+ FOR EACH ROW SET NEW.createDate = NOW()
+//
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -78,7 +107,26 @@ CREATE TABLE IF NOT EXISTS `departments` (
   `createDate` timestamp NULL DEFAULT NULL,
   `updateDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`deptID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `departments`
+--
+
+INSERT INTO `departments` (`deptID`, `deptName`, `createDate`, `updateDate`) VALUES
+(2, 'Science', '2014-04-04 01:37:14', '0000-00-00 00:00:00'),
+(3, 'Math', '2014-04-04 01:45:30', '0000-00-00 00:00:00'),
+(4, 'History', '2014-04-04 01:56:44', '0000-00-00 00:00:00');
+
+--
+-- Triggers `departments`
+--
+DROP TRIGGER IF EXISTS `departments_creation_timestamp`;
+DELIMITER //
+CREATE TRIGGER `departments_creation_timestamp` BEFORE INSERT ON `departments`
+ FOR EACH ROW SET NEW.createDate = NOW()
+//
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -89,9 +137,9 @@ CREATE TABLE IF NOT EXISTS `departments` (
 DROP TABLE IF EXISTS `submissions`;
 CREATE TABLE IF NOT EXISTS `submissions` (
   `subID` int(10) NOT NULL AUTO_INCREMENT,
-  `userID` int(10) DEFAULT NULL,
-  `deptID` int(10) NOT NULL,
-  `courseID` int(10) NOT NULL,
+  `emailAddress` text,
+  `deptName` varchar(100) NOT NULL,
+  `courseName` varchar(100) NOT NULL,
   `docName` varchar(100) NOT NULL,
   `comments` varchar(5) DEFAULT NULL,
   `instructorInstruction` varchar(5) DEFAULT NULL,
@@ -101,7 +149,7 @@ CREATE TABLE IF NOT EXISTS `submissions` (
   `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updateDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`subID`),
-  KEY `deptID` (`deptID`)
+  KEY `deptID` (`deptName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -113,26 +161,37 @@ CREATE TABLE IF NOT EXISTS `submissions` (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `userID` int(10) NOT NULL AUTO_INCREMENT,
-  `userTypeID` int(10) NOT NULL,
+  `userType` varchar(50) NOT NULL,
   `password` varchar(128) NOT NULL,
   `fname` text NOT NULL,
   `lname` text NOT NULL,
   `emailAddress` text NOT NULL,
-  `emailOptIn` tinyint(1) DEFAULT NULL,
-  `isValidated` tinyint(1) NOT NULL,
+  `emailOptIn` text,
+  `isValidated` text NOT NULL,
   `tempPassKey` varchar(128) DEFAULT NULL,
   `createDate` timestamp NULL DEFAULT '0000-00-00 00:00:00',
   `updateDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`userID`),
-  KEY `userTypeID` (`userTypeID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  KEY `userTypeID` (`userType`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userID`, `userTypeID`, `password`, `fname`, `lname`, `emailAddress`, `emailOptIn`, `isValidated`, `tempPassKey`, `createDate`, `updateDate`) VALUES
-(1, 1, '0d09e70c09a34be3114f40716eafd6b690195830d19b3aba4adb8f0cddf3634350bfe48d617d7003bd07d9b5c2039e553c5545bafc825f65d402e4caa5fbc2e3', 'Brian', 'Dunavent', 'dunavebc@mail.uc.edu', 1, 1, NULL, '2014-04-04 00:48:15', '2014-04-04 00:50:08');
+INSERT INTO `users` (`userID`, `userType`, `password`, `fname`, `lname`, `emailAddress`, `emailOptIn`, `isValidated`, `tempPassKey`, `createDate`, `updateDate`) VALUES
+(1, 'ADMIN', '0d09e70c09a34be3114f40716eafd6b690195830d19b3aba4adb8f0cddf3634350bfe48d617d7003bd07d9b5c2039e553c5545bafc825f65d402e4caa5fbc2e3', 'Brian', 'Dunavent', 'dunavebc@mail.uc.edu', 'YES', 'YES', NULL, '2014-04-04 04:48:15', '2014-04-04 04:13:35'),
+(2, 'ADMIN', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86', 'Test', 'User', 'test@test.com', 'YES', 'YES', NULL, '2014-04-04 01:33:52', '2014-04-04 04:13:46');
+
+--
+-- Triggers `users`
+--
+DROP TRIGGER IF EXISTS `users_creation_timestamp`;
+DELIMITER //
+CREATE TRIGGER `users_creation_timestamp` BEFORE INSERT ON `users`
+ FOR EACH ROW SET NEW.createDate = NOW()
+//
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -154,36 +213,8 @@ CREATE TABLE IF NOT EXISTS `usertypes` (
 --
 
 INSERT INTO `usertypes` (`userTypeID`, `userTypeName`, `createDate`, `updateDate`) VALUES
-(1, 'STANDARD', '2014-03-23 21:29:04', '2014-03-23 21:29:04'),
-(2, 'ADMIN', '2014-03-23 21:29:16', '2014-03-23 21:29:16');
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `submissions`
---
-ALTER TABLE `submissions`
-  ADD CONSTRAINT `fk_Depts_2_Submissions` FOREIGN KEY (`deptID`) REFERENCES `departments` (`deptID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `fk_UserTypes_2_Users` FOREIGN KEY (`userTypeID`) REFERENCES `usertypes` (`userTypeID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-DROP TRIGGER IF EXISTS `users_creation_timestamp`; 
-CREATE TRIGGER `users_creation_timestamp` BEFORE INSERT ON `users`
- FOR EACH ROW SET NEW.createDate = NOW();
-
-DROP TRIGGER IF EXISTS `courses_creation_timestamp`; 
-CREATE TRIGGER `courses_creation_timestamp` BEFORE INSERT ON `courses`
- FOR EACH ROW SET NEW.createDate = NOW();
-
-DROP TRIGGER IF EXISTS `departments_creation_timestamp`; 
-CREATE TRIGGER `departments_creation_timestamp` BEFORE INSERT ON `departments`
- FOR EACH ROW SET NEW.createDate = NOW();
+(1, 'STANDARD', '2014-03-24 01:29:04', '2014-03-24 01:29:04'),
+(2, 'ADMIN', '2014-03-24 01:29:16', '2014-03-24 01:29:16');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
