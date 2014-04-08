0ddce9a66223935323684ad40e9dcc5b509b5290
bdunavent <b.dunavent@gmail.com>
2014-04-08 02:15:11 -0400

    Updated Submission Profile Functionality - DB Schema change

--- Submission/submissionUpload.php
+++ Submission/submissionUpload.php
@@ -41,13 +41,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
       $body .= "Submission Profile:\n";
       $body .= "-----------------------\n\n";
       $body .= "Submission Name: {$docName}\n\n";
-      $body .= "File Name: {$submissionFile}\n\n";
       $body .= "Dept: {$dept}\n\n";
       $body .= "Course: {$course}\n\n";
       $body .= "Comments: {$comments}\n\n";
-      $body .= "Grading Rubric File: {$gradingFile}\n\n";
-      $body .= "Student Inst File: {$studentInstFile}\n\n";
-      $body .= "Instructor Inst File: {$instructorInstFile}\n\n";
 
       Users::emailOptInUsers($body);
 
@@ -108,7 +104,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
           {
             $errMsgStud = 'Student Instruction File: ' . $studentInstFile . ' upload success.';
             $studentInstFile = "<a href=\"{$fileUploadBaseDir}/{$dept}/{$studentInstFile}\">{$studentInstFile}</a>";
-            
+
           }
           else
           {
@@ -221,6 +217,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
       // insert the submission record in the database
       $isCommit = $conn -> executeQuery($insertSql);
+      $conn -> freeConnection();
+      // if inserted retrieve the record subID, and update it with the edit
+      // button
+      if ($isCommit)
+      {
+        $subID = '';
+        $editButton = '';
+        $conn = new MySqlConnect();
+        $result = $conn -> executeQueryResult("SELECT subID FROM submissions WHERE docName = '{$docName}' AND emailAddress = '{$email}' AND createDate = '{$ts}'");
+        
+        if ($row = mysql_fetch_array($result, MYSQL_ASSOC))
+        {
+          $subID = $row['subID'];
+          // get the subID from the result set
+          $editButton = '<a href="../Submission/submissionProfile.php?subID=' . $subID . '"><img width="13px" src="../Images/edit.png"></a>';
+        }
+        $conn -> freeConnection();
+        // update the record just submitted
+        $conn = new MySqlConnect();
+        $isUpdated = $conn -> executeQuery("UPDATE submissions SET edit = '{$editButton}' WHERE subID = '{$subID}'");
+        $conn -> freeConnection();
+      }
     }
     else
     {
@@ -279,73 +297,73 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 ?>
 <br />
 <form style="border:1px solid #c6bebb;" action="submissionUpload.php" method="post" accept-charset="utf-8" enctype="multipart/form-data">
-  <label for="docName">Document Name *</label>
-  <input type="hidden" id="volume" value="1" />
-  <input type="text" name="docName">
-  <br />
-  <br />
-  <label for="docFile">Document *</label> &nbsp
-  <input type="file" name="submissionfile" size="200" required="required">
-  <br>
+<label for="docName">Document Name *</label>
+<input type="hidden" id="volume" value="1" />
+<input type="text" name="docName">
+<br />
+<br />
+<label for="docFile">Document *</label> &nbsp
+<input type="file" name="submissionfile" size="200" required="required">
+<br>
 
-  <label for="comments">Document Description *</label>
-  <textarea	id="comments" name="comments" value="" wrap="virtual" 
-									rows="5em" cols="80em"
-									valign="top"
-									align="left"
-									required="required">
-	</textarea>
-  <br>
+<label for="comments">Document Description *</label>
+<textarea	id="comments" name="comments" value="" wrap="virtual"
+rows="5em" cols="80em"
+valign="top"
+align="left"
+required="required">
+</textarea>
+<br>
 
-  <label for="rubricFileName">Grading Rubric (optional)</label>
-  <input type="file" name="gradingFile" id="rubricFileName" class="clsFile">
+<label for="rubricFileName">Grading Rubric (optional)</label>
+<input type="file" name="gradingFile" id="rubricFileName" class="clsFile">
 
-  <br>
+<br>
 
-  <label for="instructionsToTheStudent">Instructions to the student (optional)</label>
-  <input type="file" name="studentInstFile" id="instructionsToTheStudent" class="clsFile">
+<label for="instructionsToTheStudent">Instructions to the student (optional)</label>
+<input type="file" name="studentInstFile" id="instructionsToTheStudent" class="clsFile">
 
-  <br>
+<br>
 
-  <label for="instructionsToTheInstructor">Instructions to the instructor (optional)</label>
-  <input type="file" name="instructorInstFile" id="instructionsToTheInstructor" class="clsFile">
+<label for="instructionsToTheInstructor">Instructions to the instructor (optional)</label>
+<input type="file" name="instructorInstFile" id="instructionsToTheInstructor" class="clsFile">
 
-  <br>
+<br>
 
-  <label for="willGrade">Will you grade assignments based on this document? &nbsp </label>
-  <input type="radio" name="willYouGrade" id="willYouGrade" value="Yes" class="radio-box" checked >
-  Yes &nbsp
-  <input type="radio" name="willYouGrade" id="willYouGrade" value="No"  class="radio-box">
-  No
-  <p>
-    <label for="department">Department *</label>
-    <?php
-    $department = Departments::getDeptList();
-    echo '<select name="department">';
-    echo '<option selected="selected">Select your department...</option>';
-    foreach ($department as $key => $value)
-    {
-      echo '<option value="' . $value . '">' . $value . '</option>';
-    }
-    echo '</select>';
+<label for="willGrade">Will you grade assignments based on this document? &nbsp </label>
+<input type="radio" name="willYouGrade" id="willYouGrade" value="Yes" class="radio-box" checked >
+Yes &nbsp
+<input type="radio" name="willYouGrade" id="willYouGrade" value="No"  class="radio-box">
+No
+<p>
+<label for="department">Department *</label>
+<?php
+$department = Departments::getDeptList();
+echo '<select name="department">';
+echo '<option selected="selected">Select your department...</option>';
+foreach ($department as $key => $value)
+{
+  echo '<option value="' . $value . '">' . $value . '</option>';
+}
+echo '</select>';
 
-    echo '<br>';
-    echo '<label for="course">Course *</label>';
+echo '<br>';
+echo '<label for="course">Course *</label>';
 
-    $course = Courses::getCourseList();
-    echo '<select name="course">';
-    echo '<option selected="selected">Select your course...</option>';
-    foreach ($course as $key => $value)
-    {
-      echo '<option value="' . $value . '">' . $value . '</option>';
-    }
-    echo '</select>';
-    ?>
-    <div class="btn-holder">
-      <button type="submit">
-        Submit
-      </button>
-    </div>
+$course = Courses::getCourseList();
+echo '<select name="course">';
+echo '<option selected="selected">Select your course...</option>';
+foreach ($course as $key => $value)
+{
+  echo '<option value="' . $value . '">' . $value . '</option>';
+}
+echo '</select>';
+?>
+<div class="btn-holder">
+<button type="submit">
+Submit
+</button>
+</div>
 </form>
 
 <?php
